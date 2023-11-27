eda704c798ad88aa3d521e52509f8a07c4af7c31
CSS17 <ofucer99@gmail.com>
2023-11-27 15:33:50 +0300

    -ViewBinding imported -Splash Screen Completed. -Location Permission on Splash Screen has completed. -User Permission added as Shared Preferences for saving the user's answer. -The dialog box only shown when the app only launch first time. -User's permission answer can go to WeatherForecast.kt

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.weatherforecast
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import com.example.weatherforecast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var  permissionResult:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        val timer = object : CountDownTimer(3000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {}
+
+            override fun onFinish() {
+                checkAndShowAlertDialog()
+            }
+        }
+
+        timer.start()
+    }
+
+    private fun checkAndShowAlertDialog() {
+        if (UserPermission.getPermission(this@MainActivity) == null) {
+            showListAlertDialog()
+
+        } else {
+            Log.d("CONTROL2","${UserPermission.getPermission(this@MainActivity)}")
+            moveToNextActivity()
+        }
+    }
+
+    private fun showListAlertDialog() {
+        val items = arrayOf("Evet", "Hayır")
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Uygulamanın konumunuza erişmesini istiyor musunuz?")
+            .setItems(items) { dialog, which ->
+                val selectedOption = items[which]
+                UserPermission.savePermission(this, selectedOption)
+                dialog.dismiss()
+
+                // Dialog kutusu işlemleri tamamlandıktan sonra diğer aktiviteye geçiş yap
+                Log.d("CONTROL1","${UserPermission.getPermission(this@MainActivity)}")
+                moveToNextActivity()
+            }
+
+        builder.show()
+    }
+
+    private fun moveToNextActivity() {
+        val intent = Intent(this@MainActivity, WeatherForecast::class.java)
+        permissionResult=UserPermission.getPermission(this@MainActivity).toString()
+        Log.d("CONTROL3",permissionResult)
+        intent.putExtra("PERMISSION", permissionResult)
+        startActivity(intent)
+        finish()
     }
 }
---
+++ app/src/main/java/com/example/weatherforecast/UserPermission.kt
@@ -0,0 +1,23 @@
+package com.example.weatherforecast
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object UserPermission {
+
+    private const val PERMISSION = "PERMISSION"
+
+    private fun getSharedPreferences(context: Context): SharedPreferences {
+        return context.getSharedPreferences(PERMISSION, Context.MODE_PRIVATE)
+    }
+
+    fun savePermission(context: Context, permission: String) {
+        getSharedPreferences(context).edit().putString(PERMISSION, permission).apply()
+    }
+
+    fun getPermission(context: Context): String? {
+        return getSharedPreferences(context).getString(PERMISSION, null)
+    }
+
+
+}
